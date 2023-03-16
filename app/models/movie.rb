0a655e7eec7905a56f9af6cf15d8d99f86eb57fa
teenyTECHmama <116871259+teenyTECHmama@users.noreply.github.com>
2023-03-16 13:02:58 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,10 +2,7 @@ class Movie < ActiveRecord::Base
     def self.all_ratings
       ['G','PG','PG-13','R']
     end
-
-    
-      
-
+  
     def self.with_ratings(ratings_list)
       # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
       #  movies with those ratings
